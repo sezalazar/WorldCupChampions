e739f3b9597e830552967acdcd13ac5cffe2d490
sezalazar <53580787+sezalazar@users.noreply.github.com>
2020-04-18 02:13:30 -0300

    Add files via upload

---
+++ database.php
@@ -0,0 +1,41 @@
+<?php 
+
+// Parameters: Host, User, Pass, Database Name
+
+function connect(){
+
+$link =  mysqli_connect('localhost', 'youtube', 'uqgdk25xCalNScWp', 'worldcupchampions');
+
+return $link;
+
+}
+
+function listChampions(){
+
+$link = connect();
+
+$sql = "select year, champion from champions";
+
+$result = mysqli_query($link, $sql);
+
+return $result;
+
+}
+
+
+function addChampion(){
+
+$year = $_POST['yearpost'];
+$champ = $_POST['championpost'];
+
+$link = connect();
+
+$sql = "INSERT INTO CHAMPIONS (YEAR, CHAMPION) VALUES('" . $year  . "' , '" . $champ . "'  )";
+
+$result = mysqli_query($link, $sql);
+
+return $result;
+
+}
+
+?>
